a91e5b652dd910b493620b6aed13488059e7c616
Ariea Sampat <196046875+ARIEA-hub@users.noreply.github.com>
2026-09-16 00:38:37 +0530

    Add Spring Boot main application class

---
+++ src/main/java/com/wondrx/txledger/TxLedgerApplication.java
@@ -0,0 +1,11 @@
+package com.wondrx.txledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxLedgerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TxLedgerApplication.class, args);
+    }
+}
